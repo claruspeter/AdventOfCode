75e4bdd37651c8ac3aec7ef3f1704b40c9ad5132
peterb <prbayne@gmail.com>
2019-12-11 17:32:27 +1300

    Day5 started

--- Program.fs
+++ Program.fs
@@ -5,8 +5,12 @@ open System
 let main argv =
     // printfn "AOC 1a: %d" AOC1.data
     // printfn "AOC 1b: %d" AOC1.data_b
-    // AOC2.generate1202Error
-    // printfn "%A" AOC2.findTarget
+    // printfn "AOC 2a test1: %A" (AOC2.test1())
+    // printfn "AOC 2a test2: %i" (AOC2.test2())
+    // printfn "AOC 2a test3: %i" (AOC2.test3())
+    // printfn "AOC 2a test4: %i" (AOC2.test4())
+    // AOC2.generate1202Error()
+    // printfn "%A" (AOC2.findTarget())
     // printfn "AOC 3a test1: %i" (AOC3.minDistance 0 1)
     // printfn "AOC 3a test2: %i" (AOC3.minDistance 2 3)
     // printfn "AOC 3a test3: %i" (AOC3.minDistance 4 5)
@@ -15,6 +19,9 @@ let main argv =
     // printfn "AOC 3b test1: %i" (AOC3.minPath 2 3)
     // printfn "AOC 3b test1: %i" (AOC3.minPath 4 5)
     // printfn "AOC 3b test1: %i" (AOC3.minPath 6 7)
-    printfn "AOC 4a: %d" AOC4.partA
+    // printfn "AOC 4a: %d" AOC4.partA
+
+    printfn "AOC 5a test1: %A" (AOC5.test1())
+
 
     0 
--- aoc2.fs
+++ aoc2.fs
@@ -1,5 +1,6 @@
 module AOC2
 
+open System
 open AOC_Common
 
 let printProgram (ints: int []) =
@@ -7,11 +8,13 @@ let printProgram (ints: int []) =
     |> Array.chunkBySize 4
     |> Array.map (fun x -> if x.Length = 4 then x else Array.append x [|-1; -1; -1; -1|] |> Array.take 4 )
     |> Array.iteri (fun i x -> printfn "%4i: %4i %4i %4i %4i" (i*4) x.[0] x.[1] x.[2] x.[3])
+    ints
 
 
-let inputs = 
+let inputs() = 
     (raw 2).Split([|','|])
     |> Array.choose parseInts
+
 
 
 let doctor noun verb (ints: int []) = 
@@ -20,43 +23,102 @@ let doctor noun verb (ints: int []) =
     result.[2] <- verb 
     result
 
-let rec processIntMachine position (ints: int [])  =
-    let a = ints.[position + 1]
-    let aValue = ints.[a]
-    let b = ints.[position + 2]
-    let bValue = ints.[b]
-    let at = ints.[position + 3]
-    // printfn "PROG %4i: %4i %4i(%4i) %4i(%4i) %4i" position ints.[position] a aValue b bValue at
-    match ints.[position] with
-    | 1 -> 
-        let result = ints |> Array.copy
-        result.[at] <- aValue + bValue
-        processIntMachine (position + 4) result 
-    | 2 -> 
-        let result = ints |> Array.copy
-        result.[at] <- aValue * bValue
-        processIntMachine (position + 4) result 
-    | 99 -> 
-        // End
-        ints
-    | _ -> failwithf "Unkown Command: %i" ints.[position]
 
-let generate1202Error = 
-    inputs
+type ParaValue = {
+    value: int
+    valueAtPosition: int
+} with
+    override this.ToString() = sprintf "{%i -> %i}" this.value this.valueAtPosition
+
+type IntParams = {
+    destination: int
+    parameters: ParaValue list
+}
+
+type CmdProcessor = int[] -> IntParams -> int[]
+type Cmd = {
+    processor: CmdProcessor
+    numberOfParameters: int
+}
+
+
+let processCmd1 ints paras = 
+    let result = ints |> Array.copy
+    result.[paras.destination] <- paras.parameters.[0].valueAtPosition + paras.parameters.[1].valueAtPosition
+    result
+
+let processCmd2 ints paras = 
+    let result = ints |> Array.copy
+    result.[paras.destination] <- paras.parameters.[0].valueAtPosition * paras.parameters.[1].valueAtPosition
+    result
+
+let TwoCommands = [
+    { processor = processCmd1; numberOfParameters=2 }
+    { processor = processCmd2; numberOfParameters=2 }
+]
+
+let rec processIntMachine (commandProcessors: Cmd list) position (ints: int [])  =
+    let cmdIndex = ints.[position]
+    printfn "CMD %i at %i" cmdIndex position
+    if cmdIndex = 99 then 
+        ints
+    else
+        let cmd = commandProcessors.[cmdIndex - 1]
+        printfn "-paras: %i" cmd.numberOfParameters
+        let paraValues = 
+            [1..cmd.numberOfParameters]
+            |> List.map ( fun x -> 
+                let v = ints.[x + position]
+                {
+                    value=v; 
+                    valueAtPosition=ints.[v]
+                }
+            )
+        paraValues |> List.iteri (fun i x -> printfn "--para: %i=%O" (i + position) x)
+        let paras = {
+            destination = ints.[position + cmd.numberOfParameters + 1 ]
+            parameters = paraValues
+        }
+        let result = cmd.processor ints paras
+        processIntMachine commandProcessors (position + cmd.numberOfParameters + 2) result 
+
+let test1() = 
+    [|1;0;0;0;99|]
+    |> processIntMachine TwoCommands 0
+    |> Array.compareWith (fun a b -> a.CompareTo b) [|2;0;0;0;99|]
+
+let test2() = 
+    [|2;3;0;3;99|]
+    |> processIntMachine TwoCommands 0
+    |> Array.compareWith (fun a b -> a.CompareTo b) [|2;3;0;6;99|]
+
+let test3() = 
+    [|2;4;4;5;99;0|]
+    |> processIntMachine TwoCommands 0
+    |> Array.compareWith (fun a b -> a.CompareTo b) [|2;4;4;5;99;9801|]
+
+let test4() = 
+    [|1;1;1;4;99;5;6;0;99|]
+    |> processIntMachine TwoCommands 0
+    |> Array.compareWith (fun a b -> a.CompareTo b) [|30;1;1;4;2;5;6;0;99|]
+
+let generate1202Error() = 
+    inputs()
     |> doctor 12 02
-    |> processIntMachine 0
+    |> processIntMachine TwoCommands 0
     |> printProgram
+    |> ignore
 
 let target = 19690720
 
-let findTarget = 
+let findTarget() = 
     let run = 
         [for noun in [0..99] do
             for verb in [0..99] do
                 let result = 
-                    inputs
+                    inputs()
                     |> doctor noun verb
-                    |> processIntMachine 0
+                    |> processIntMachine TwoCommands 0
                     |> (fun x -> x.[0])
                 yield {| noun=noun; verb=verb; result=result; success=(result = target)|}
         ]
