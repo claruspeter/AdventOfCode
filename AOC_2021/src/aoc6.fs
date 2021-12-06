1b8fdc9592eac579d42c630767473f09a8a04945
Peter Bayne <peter.bayne@haumohio.com>
2021-12-07 11:06:39 +1300

    2021 Day 6

--- AOC_2021/src/aoc6.fs
+++ AOC_2021/src/aoc6.fs
@@ -10,3 +10,40 @@ let nextDay fish =
       | 0 -> [6;8]
       | _ -> [n-1]
   )
+
+type Fish = {
+  countdowns : int64[]
+}
+
+let initFish (fishlist: int seq) = 
+  let counts = 
+    fishlist 
+    |> Seq.countBy id
+  { countdowns = 
+    [|0..8|]
+    |> Array.map (
+        fun i ->
+          counts 
+          |> Seq.tryFind (fun (idx, _) -> idx = i) 
+          |> Option.map (snd)
+          |> Option.defaultValue 0
+          |> int64 
+      )
+  }
+
+let incDay (fish: Fish): Fish =
+  /// Sort of a shift/rotate left plus injection of zeroes at #6
+  let numZeroes = fish.countdowns.[0]
+  {
+    countdowns = [|
+      fish.countdowns.[1] //--> 0
+      fish.countdowns.[2] //--> 1
+      fish.countdowns.[3] //--> 2
+      fish.countdowns.[4] //--> 3
+      fish.countdowns.[5] //--> 4
+      fish.countdowns.[6] //--> 5
+      fish.countdowns.[7] + numZeroes //--> 6
+      fish.countdowns.[8] //--> 7
+      numZeroes //--> 8
+    |]
+  }
--- AOC_2021/tests/test6.fs
+++ AOC_2021/tests/test6.fs
@@ -31,3 +31,34 @@ let A () =
     [1..80] 
     |> Seq.fold (fun acc _ -> nextDay acc ) data
   day80 |> Seq.length |> should equal 390923
+
+
+[<Fact>]
+let B_Sample () =
+  let data = sample.Split([|','|]) |> Seq.map Int32.Parse
+  let fish = initFish data
+  let day18 = 
+    [1..18] 
+    |> Seq.fold (fun acc _ -> incDay acc ) fish
+  day18.countdowns |> Seq.sum |> should equal 26L
+  let day80 = 
+    [1..80] 
+    |> Seq.fold (fun acc _ -> incDay acc ) fish
+  day80.countdowns |> Seq.sum |> should equal 5934L
+  let day256 = 
+    [1..256] 
+    |> Seq.fold (fun acc _ -> incDay acc ) fish
+  day256.countdowns |> Seq.sum |> should equal 26984457539L
+
+[<Fact>]
+let B () =
+  let data = (raw 6).Split([|','|]) |> Seq.map Int32.Parse
+  let fish = initFish data
+  let day80 = 
+    [1..80] 
+    |> Seq.fold (fun acc _ -> incDay acc ) fish
+  day80.countdowns |> Seq.sum |> should equal 390923L
+  let day256 = 
+    [1..256] 
+    |> Seq.fold (fun acc _ -> incDay acc ) fish
+  day256.countdowns |> Seq.sum |> should equal 1749945484935L
