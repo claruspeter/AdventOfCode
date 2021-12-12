302586afc7573b3af6ad47bae9dc1293fdf66d37
Peter Bayne <peter.bayne@haumohio.com>
2021-12-13 09:05:25 +1300

    2021 Day 10

--- AOC_2021/src/aoc10.fs
+++ AOC_2021/src/aoc10.fs
@@ -3,57 +3,6 @@ module AOC2021.Day10
 open System
 open AOC2021.Common
 
-type Corruption = {
-  expected: char
-  found: char
-}
-with 
-  member this.value = 
-    match this.found with 
-    | ')' -> 3
-    | ']' -> 57
-    | '}' -> 1197
-    | '>' -> 25137
-    | _ -> failwith "unknown"
-
-type Chunk = {
-  value: char
-  child: Chunk[]
-}
-
-type LineCheck = Result<Chunk[], Corruption>
-
-let symbols = "([{<)]}>"
-
-let closedBy closing opening =
-  match  opening, closing with
-  | '(', ')' -> true
-  | '[', ']' -> true
-  | '{', '}' -> true
-  | '<', '>' -> true
-  | _ -> false
-
-let space = "........................................"
-
-let rec private parseChunks' (depth: int) (parent: char) (line:string) =
-  if line = "" then 
-    {expected=parent; found=' '} |> Error
-  else
-    match line.[0] |> symbols.IndexOf with 
-    | n when n < 4 -> 
-      //opening
-      printfn "%s %c" (space.Substring(0,depth)) line.[0]
-      parseChunks' (depth+1) line.[0] (line.Substring 1)
-    | n -> 
-      //closing
-      printfn "%s %c" (space.Substring(0,depth-1)) line.[0]
-      if parent |> closedBy line.[0]  then 
-        [| {value=parent; child=[||]} |] |> Ok
-      else 
-        {expected=parent; found=line.[0] } |> Error
-
-let parseChunks (line:string) = parseChunks' 0 (line.[0]) (line.Substring(1))
-
 let private findIllegal' (openers: char list) (current:char) =
   let head = if openers = [] then ' ' else openers.[0]
   match head, current with 
@@ -81,3 +30,10 @@ let findIllegal (s:string) =
     | ">" -> 25137
     | _ -> 0
     |> Error
+
+let private _scores = [' '; '('; '['; '{'; '<']
+
+let scoreClosers (openers: char list) =
+  openers
+  |> Seq.map (fun c -> _scores |> List.findIndex ((=) c) |> int64 )
+  |> Seq.fold (fun acc i -> acc * 5L + i ) 0L
--- AOC_2021/tests/test10.fs
+++ AOC_2021/tests/test10.fs
@@ -44,7 +44,41 @@ let A () =
       | Ok _ -> None
     )
     |> Seq.sum
-  result |> should equal 26397
+  result |> should equal 344193
 
+[<Fact>]
+let B_Sample () =
+  let result = 
+    sample
+    |> Seq.map findIllegal
+    |> Seq.choose (fun x -> 
+      match x with
+      | Error _ -> None
+      | Ok a -> Some a
+    )
+  let scores = 
+    result
+    |> Seq.map scoreClosers
+    |> Seq.toList
+  scores |> should equal [288957L; 5566L; 1480781L; 995444L; 294L]
+  scores 
+  |> List.sort 
+  |> fun data -> data.[data.Length / 2] 
+  |> should equal 288957L
 
-
+[<Fact>]
+let B () =
+  let result = 
+    lines 10
+    |> Seq.map findIllegal
+    |> Seq.choose (fun x -> 
+      match x with
+      | Error _ -> None
+      | Ok a -> Some a
+    )
+  result
+    |> Seq.map scoreClosers
+    |> Seq.sort 
+    |> Seq.toList
+    |> fun data -> data.[data.Length / 2] 
+    |> should equal 3241238967L
