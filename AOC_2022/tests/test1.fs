bb643e7c2a6d9db711cb074938595b015647ab32
Peter Bayne <peter.bayne@haumohio.com>
2022-12-03 14:43:53 +1300

    Day 1 complete

--- AOC_2022/tests/test1.fs
+++ AOC_2022/tests/test1.fs
@@ -24,7 +24,6 @@ let A_Sample () =
     ""
     "10000"
   ]
-
   let byElf = input |> groupCalories
   byElf |> List.length |> should equal 5
   let sums = byElf |> List.map (fun elf -> elf |> List.sum) 
@@ -33,9 +32,37 @@ let A_Sample () =
 [<Fact>]
 let A () =
   let input = lines 1
-
   let byElf = input |> groupCalories
   let sums = byElf |> Seq.map (fun elf -> elf |> Seq.sum) 
-  sums |> Seq.max |> should equal 1L
+  sums |> Seq.max |> should equal 70116L
 
 
+[<Fact>]
+let B_Sample () =
+  let input = [
+    "1000"
+    "2000"
+    "3000"
+    ""
+    "4000"
+    ""
+    "5000"
+    "6000"
+    ""
+    "7000"
+    "8000"
+    "9000"
+    ""
+    "10000"
+  ]
+  let byElf = input |> groupCalories
+  byElf |> List.length |> should equal 5
+  let sums = byElf |> List.map (fun elf -> elf |> List.sum) |> List.sort
+  sums |> should equal [4000L; 6000L; 10000L; 11000L; 24000L]
+
+[<Fact>]
+let B () =
+  let input = lines 1
+  let byElf = input |> groupCalories
+  let sums = byElf |> Seq.map (fun elf -> elf |> Seq.sum) 
+  sums |> Seq.sortDescending |> Seq.take 3 |> Seq.sum |> should equal 206582L
