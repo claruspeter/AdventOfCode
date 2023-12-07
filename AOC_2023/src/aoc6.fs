da2f4ff687d6d0a2231f8ff3add5636337aab472
Peter Bayne <peter.bayne@haumohio.com>
2023-12-07 15:10:35 +1300

    Day 6 with speed up

--- AOC_2023/src/aoc6.fs
+++ AOC_2023/src/aoc6.fs
@@ -7,13 +7,14 @@ type private Race = {
   time: int64; 
   distance: int64
 }with 
-  member this.WinningDistances =
-    [1L..this.time]
-    |> List.choose (fun speed -> 
-        match speed * (this.time - speed) with 
-        | d when d > this.distance -> Some d
-        | _ -> None
-    )
+  member this.WinningDistanceCount =
+    // The calculation is symmetric.  So the number of "losers" is equal at the top and bottom ends
+    seq [1L..this.time] 
+    |> Seq.takeWhile (fun speed -> speed * (this.time - speed) <= this.distance)
+    |> Seq.length
+    |> int64
+    |> fun nLowLosers -> this.time - (nLowLosers * 2L) - 1L
+    |> int
 
 let private parseRaceLineWithSpaces (line:string) =
   line.Substring(10).Split([|' '|], StringSplitOptions.RemoveEmptyEntries) |> Array.toList
@@ -28,13 +29,14 @@ let private parseRaces lineParser (lines:string list) =
       | [a;b] -> List.zip a b
       | x -> failwithf "Impossible! %A" x
   |> List.map (fun (a, b) -> {time=int64 a; distance=int64 b})
-  |> List.map (fun race -> race.WinningDistances |> List.length )
 
 
 let numberWaysToWin (lines:string list) =
   lines
   |> parseRaces parseRaceLineWithSpaces
+  |> List.map (fun race -> race.WinningDistanceCount )
 
 let numberWaysToWinLong (lines:string list) =
   lines
   |> parseRaces parseRaceLineWithoutSpaces
+  |> List.map (fun race -> race.WinningDistanceCount )
