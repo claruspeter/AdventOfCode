4622c9c66bcf74f3e9cfb36f6529c27633a368fc
Peter Bayne <peter.bayne@haumohio.com>
2023-12-08 15:16:05 +1300

    Day 7

--- AOC_2023/src/aoc7.fs
+++ AOC_2023/src/aoc7.fs
@@ -27,41 +27,70 @@ type HandType =
   | FourOfAKind   = 6
   | FiveOfAKind   = 7 
 
+let private promoteHandTypeWithWildcards n handType =
+  if n > 0  then 
+    let simpleCountHands = [HandType.HighCard; HandType.OnePair; HandType.ThreeOfAKind; HandType.FourOfAKind; HandType.FiveOfAKind]
+    let clamped = Math.Min(n, 4)
+    match handType with
+    | HandType.HighCard     -> simpleCountHands.[Math.Max(4, clamped)]
+    | HandType.OnePair      -> simpleCountHands.[Math.Max(4, clamped+1)]
+    | HandType.TwoPair      -> HandType.FullHouse
+    | HandType.ThreeOfAKind -> simpleCountHands.[Math.Max(4, clamped+2)]
+    | HandType.FullHouse    -> HandType.FullHouse
+    | HandType.FourOfAKind  -> HandType.FiveOfAKind
+    | HandType.FiveOfAKind  -> HandType.FiveOfAKind
+    | x -> failwithf "Not Possible %A" x
+  else
+    handType
+
 type Hand = {
   cards: string
   bid: int
+  wildCard: char
 }with 
-  member hand.handType =
-    let histoValues = 
-      hand.cards
-      |> histogram 
+  member hand.handType  =
+    let hist = hand.cards |> histogram 
+    let nWild = hist.OrDefault 0 hand.wildCard
+    let histValues = 
+      hist
+      |> fun h -> 
+          h.Remove(hand.wildCard) |> ignore
+          h 
       |> fun x -> x.Values 
       |> Seq.sortDescending
       |> Seq.toList
-    match histoValues with 
+    match histValues with 
     | [5] -> HandType.FiveOfAKind
-    | [4; 1] -> HandType.FourOfAKind
+    | 4::_ -> HandType.FourOfAKind
     | [3; 2] -> HandType.FullHouse
-    | [3; 1; 1] -> HandType.ThreeOfAKind
+    | 3:: _ -> HandType.ThreeOfAKind
+    | [2; 2]  
     | [2; 2; 1] -> HandType.TwoPair
     | 2::_ -> HandType.OnePair
     | _ -> HandType.HighCard
+    |> promoteHandTypeWithWildcards nWild
 
 
 let private handComparer (a:Hand) (b:Hand) =
+  let cardValuesWithWildCard = sprintf "%c%s" a.wildCard CardValues
   match a.handType, b.handType with 
   | x, y when x <> y -> x.CompareTo(y)
   | _ -> 
-      let aValues = a.cards |> Seq.map (CardValues.IndexOf)
-      let bValues = b.cards |> Seq.map (CardValues.IndexOf)
+      let aValues = a.cards |> Seq.map (cardValuesWithWildCard.IndexOf)
+      let bValues = b.cards |> Seq.map (cardValuesWithWildCard.IndexOf)
       Seq.compareWith (fun (aV:int) (bV:int) -> aV.CompareTo(bV)) aValues bValues
 
-let private parseHand (line: string) =
+let private parseHand wildCard (line: string) =
   match line.Split(' ')with 
-  | [|cards; bid|] -> {cards=cards; bid=int bid} |> Some
+  | [|cards; bid|] -> {cards=cards; bid=int bid; wildCard=wildCard} |> Some
   | _ -> None
 
 let parseAndSortHands (lines: string list) =
   lines
-  |> List.choose parseHand
+  |> List.choose (parseHand '_')
+  |> List.sortWith handComparer
+
+let parseAndSortHandsWithJacksWild (lines: string list) =
+  lines
+  |> List.choose (parseHand 'J')
   |> List.sortWith handComparer
--- AOC_2023/tests/test7.fs
+++ AOC_2023/tests/test7.fs
@@ -18,16 +18,28 @@ let sample = [
 let A_Sample () =
   sample
   |> parseAndSortHands
-  |>List.mapi (fun i hand -> (hand.bid, i + 1))
+  |> List.mapi (fun i hand -> (hand.bid, i + 1))
   |> should equal [(765,1); (220,2); (28,3); (684,4); (483,5)]
 
 [<Fact>]
 let A () =
   lines 7
   |> parseAndSortHands
-  |>List.mapi (fun i hand -> hand.bid * (i + 1))
+  |> List.mapi (fun i hand -> hand.bid * (i + 1))
   |> List.sum
   |> should equal 250347426
 
+[<Fact>]
+let B_Sample () =
+  sample
+  |> parseAndSortHandsWithJacksWild
+  |> List.mapi (fun i hand -> (hand.bid, i + 1))
+  |> should equal [(765,1); (28,2); (684,3); (483,4); (220,5)]
 
-
+[<Fact>]
+let B () =
+  lines 7
+  |> parseAndSortHandsWithJacksWild
+  |> List.mapi (fun i hand -> hand.bid * (i + 1))
+  |> List.sum
+  |> should equal 253134644
