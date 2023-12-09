d087f40489f4fcc6eac18ff0a27cce2f1bf88efa
Peter Bayne <peter.bayne@haumohio.com>
2023-12-09 16:21:15 +1300

    Day 7b - get a stack overflow
    from infinite loop - never reach all nodes anding in a 'Z'

--- AOC_2023/src/aoc8.fs
+++ AOC_2023/src/aoc8.fs
@@ -4,21 +4,22 @@ open System
 open System.Collections.Generic
 open AOC2023.Common
 
-type private NetworkNode = {
+type NetworkNode = {
   name: string
   left: string
   right: string
 }
 
-type private Network = {
+type Network = {
   leftRight : char list
   nodes: IDictionary<string,NetworkNode>
 }
 
 let private parseNetworkNode line =
   match line with 
-  | Regex "([A-Z]{3}) = \\(([A-Z]{3}), ([A-Z]{3})\\)" [_; a; b; c] -> {name=a; left=b; right=c} |> Some
+  | Regex "([A-Z0-9]{3}) = \\(([A-Z0-9]{3}), ([A-Z0-9]{3})\\)" [_; a; b; c] -> {name=a; left=b; right=c} |> Some
   | _ -> None
+
 
 let private parseNetwork (lines: string list) =
   let nodes = 
@@ -38,18 +39,31 @@ let private branch network currentNode leftRight =
   | 'R' -> network.nodes.[currentNode.right]
   | _ -> failwithf "Impossible! %A" leftRight
 
-let rec private walk currentNode lrIndex network =
-  match currentNode.name with 
-  | "ZZZ" -> ["ZZZ"]
-  | name -> 
-    let nextNode = branch network currentNode network.leftRight.[lrIndex]
-    walk nextNode ((lrIndex + 1) % network.leftRight.Length) network
-    |> (@) [name]
+let rec private walk currentNodes lrIndex network =
+  match currentNodes |> List.map (fun x -> x.name) with 
+  | x when x |> List.forall (fun name -> name.EndsWith "Z") -> [x]
+  | names -> 
+    let nextNodes = currentNodes |> List.map (fun node -> branch network node network.leftRight.[lrIndex])
+    let nextLR = (lrIndex + 1) % network.leftRight.Length
+    walk nextNodes nextLR network
+    |> (@) [names]
 
-let private walkNetwork network =
-  walk (network.nodes.["AAA"]) 0 network
+let private walkNetwork starters network =
+  let startingNodes = network |> starters
+  startingNodes |> List.map (fun x -> x.name) |> printfn "Starting with %A" 
+  walk startingNodes 0 network
 
-let walkThePath (lines: string list ) =
+let starterGhost network = 
+  network.nodes 
+  |> Seq.filter (fun x -> x.Key.EndsWith "A") 
+  |> Seq.map (fun x -> x.Value) 
+  |> Seq.toList
+
+let starterSimple network = 
+  network.nodes["AAA"]
+  |> List.singleton
+
+let walkThePath starters (lines: string list ) =
   lines
   |> parseNetwork 
-  |> walkNetwork
+  |> walkNetwork starters
--- AOC_2023/tests/test8.fs
+++ AOC_2023/tests/test8.fs
@@ -29,23 +29,58 @@ let sample2 = [
 [<Fact>]
 let A_Sample () =
   sample
-  |> walkThePath
+  |> walkThePath starterSimple
+  |> List.collect id
   |> should equal ["AAA"; "CCC"; "ZZZ"]
 
 [<Fact>]
 let A_Sample2 () =
   sample2
-  |> walkThePath
+  |> walkThePath starterSimple
+  |> List.collect id
   |> should equal ["AAA"; "BBB"; "AAA"; "BBB"; "AAA"; "BBB"; "ZZZ"]
 
 [<Fact>]
 let A () =
   lines 8
-  |> walkThePath
+  |> walkThePath starterSimple
+  |> List.collect id
   |> Seq.length
   |> (+) -1
   |> should equal 11567
 
+let sampleB = [
+  "LR"
+  ""
+  "11A = (11B, XXX)"
+  "11B = (XXX, 11Z)"
+  "11Z = (11B, XXX)"
+  "22A = (22B, XXX)"
+  "22B = (22C, 22C)"
+  "22C = (22Z, 22Z)"
+  "22Z = (22B, 22B)"
+  "XXX = (XXX, XXX)"
+]
+
+[<Fact>]
+let B_Sample () =
+  sampleB
+  |> walkThePath starterGhost
+  |> should equal [
+      ["11A"; "22A"]
+      ["11B"; "22B"]
+      ["11Z"; "22C"]
+      ["11B"; "22Z"]
+      ["11Z"; "22B"]
+      ["11B"; "22C"]
+      ["11Z"; "22Z"]
+    ]
 
 
-
+// [<Fact>]
+// let B () =
+//   lines 8
+//   |> walkThePath starterGhost
+//   |> Seq.length
+//   |> (+) -1
+//   |> should equal 1
