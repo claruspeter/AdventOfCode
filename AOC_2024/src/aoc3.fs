e9b32fcd938e0f1ae615d075b3f2774af253ff89
Peter Bayne <peter.bayne@haumohio.com>
2024-12-05 14:17:14 +1300

    Day 3, stuck on part B

--- AOC_2024/src/aoc3.fs
+++ AOC_2024/src/aoc3.fs
@@ -3,6 +3,9 @@ module AOC2023.Day3
 open System
 open AOC2023.Common
 
+let private _do = "do()"
+let private _dont = "don't()"
+
 let extractMuls (line:string) = 
   match line with 
   | Regex @"mul\((\d+),(\d+)\)" x -> 
@@ -12,3 +15,13 @@ let extractMuls (line:string) =
   | _ -> []
 
 let calcMul (a,b) = a*b
+
+let private removeDont (line: string) =
+  line.Split(_dont, StringSplitOptions.RemoveEmptyEntries)
+  |> Array.head
+
+
+let splitConditional (line: string) =
+  line.Split(_do, StringSplitOptions.RemoveEmptyEntries)
+  |> Array.map removeDont
+  |> Array.toList
--- AOC_2024/tests/test3.fs
+++ AOC_2024/tests/test3.fs
@@ -8,6 +8,7 @@ open AOC2023.Day3
 
 
 let sample = ["xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"]
+let sampleB = ["xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"]
 
 [<Fact>]
 let A_Sample () =
@@ -29,5 +30,24 @@ let A () =
     |> List.sum
     |> should equal 153469856
 
+[<Fact>]
+let B_Sample () =
+  let muls = 
+    sampleB
+    |> List.collect splitConditional
+    |> List.collect extractMuls
+  muls
+    |> should equal [(2,4); (8,5)] 
+  muls
+    |> List.map calcMul
+    |> List.sum
+    |> should equal 48
 
-
+[<Fact>]
+let B () =
+  lines 3
+    |> List.collect splitConditional
+    |> List.collect extractMuls
+    |> List.map calcMul
+    |> List.sum
+    |> should equal 153469856
